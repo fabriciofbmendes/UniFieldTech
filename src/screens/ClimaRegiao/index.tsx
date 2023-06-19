2831754de18c5053e9a9ae1e6a1c0141f7a69d22
Fabricio Terra <lima.de.terra@hotmail.com>
2023-06-18 21:49:38 -0300

    Adicionando funções para varias cidades
    Adicionando funcões para varias o usuario adicionar as cidades que ele deseja observar a temperatura

--- src/screens/ClimaRegiao/index.tsx
+++ src/screens/ClimaRegiao/index.tsx
@@ -1,37 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import { useNavigation, useRoute } from '@react-navigation/native';
 
 const ClimaRegiao = () => {
   const route = useRoute();
-  const [data,setData] = useState({});
+  const [data, setData] = useState({});
   const [temperature, setTemperature] = useState(null);
+  const [closestCities, setClosestCities] = useState([]);
+  const [location, setLocation] = useState('');
 
   useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&hourly=temperature_2m&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo`;
 
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=-21.45&longitude=-45.95&hourly=temperature_2m&current_weather=true&forecast_days=1&timezone=America%2FSao_Paulo`;
-    const fetchTemperature = async () => {
       try {
-        axios.get(url).then((response) => {
-          setData(response.data)
-          console.log(response.data)
-        });
+        const response = await axios.get(url);
+        setData(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error('Error fetching temperature:', error);
       }
     };
 
-    fetchTemperature();
-  }, []);
+    // Chama a função fetchData inicialmente
+    fetchData();
+
+    // Chama a função fetchData novamente quando a localização é atualizada
+  }, [location]);
+
+  const handleLocationSubmit = () => {
+    // Realiza alguma validação antes de atualizar a localização
+    // e chamar fetchData novamente
+    setLocation({ latitude: 0, longitude: 0 }); // Atualiza a localização com os valores corretos
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Latitude: {-21.4}, Longitude: {-45.95}
+      <Text style={styles.headerText}>
+        Latitude: -21.4, Longitude: -45.95
       </Text>
-        <Text style={styles.text}>{data.hourly.temperature_2m}</Text>
-      
+      <View style={styles.temperatureContainer}>
+        <Text style={styles.temperatureText}>{data.current_weather?.temperature}</Text>
+      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a localização"
+        value={location}
+        onChangeText={setLocation}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLocationSubmit}>
+        <Text style={styles.buttonText}>Buscar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -41,10 +61,43 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 18,
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  temperatureContainer: {
+    borderWidth: 2,
+    borderRadius: 8,
+    padding: 10,
+    borderColor: 'black',
+    marginBottom: 20,
+  },
+  temperatureText: {
+    fontSize: 48,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 10,
     marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
