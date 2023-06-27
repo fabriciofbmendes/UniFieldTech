eb3fccb0f023248fc80d7888f934b55f7280cd11
FabricioTLima <lima.de.terra@hotmail.com>
2023-06-26 23:38:04 -0300

    Update validacoes.ts

--- src/screens/CalendarioClima/validacoes.ts
+++ src/screens/CalendarioClima/validacoes.ts
@@ -33,7 +33,6 @@ const VerificaVento = async (response:string) => {
             case "arroz":
                 for(let i = 0;i < 7; i++){
                     const windspeedMax = ClimaValidacao.daily?.windspeed_10m_max?.[i] || '0';
-                    console.log("Dia: " + i + " Velocidade do Vento2:" + parseFloat(windspeedMax));
                     if (parseFloat(windspeedMax) < 40.0) {
                         PerigoVento[i] = "verde";
                         console.log("PerigoVento:" + PerigoVento[i]);
@@ -47,12 +46,60 @@ const VerificaVento = async (response:string) => {
                 }
                 break;
             case "soja":
+                for(let i = 0;i < 7; i++){
+                    const windspeedMax = ClimaValidacao.daily?.windspeed_10m_max?.[i] || '0';
+                    if (parseFloat(windspeedMax) < 40.0) {
+                        PerigoVento[i] = "verde";
+                    }
+                    else if (parseFloat(windspeedMax) > 40.0 && parseFloat(windspeedMax) <= 50.0) {
+                        PerigoVento[i] = "amarelo";
+                    }
+                    else {
+                        PerigoVento[i] = "vermelho";
+                    }
+                }
                 break;
             case "milho":
+                for(let i = 0;i < 7; i++){
+                    const windspeedMax = ClimaValidacao.daily?.windspeed_10m_max?.[i] || '0';
+                    if (parseFloat(windspeedMax) < 60.0) {
+                        PerigoVento[i] = "verde";
+                    }
+                    else if (parseFloat(windspeedMax) > 60.0 && parseFloat(windspeedMax) <= 70.0) {
+                        PerigoVento[i] = "amarelo";
+                    }
+                    else {
+                        PerigoVento[i] = "vermelho";
+                    }
+                }
                 break;
             case "trigo":
+                for(let i = 0;i < 7; i++){
+                    const windspeedMax = ClimaValidacao.daily?.windspeed_10m_max?.[i] || '0';
+                    if (parseFloat(windspeedMax) < 60.0) {
+                        PerigoVento[i] = "verde";
+                    }
+                    else if (parseFloat(windspeedMax) > 60.0 && parseFloat(windspeedMax) <= 70.0) {
+                        PerigoVento[i] = "amarelo";
+                    }
+                    else {
+                        PerigoVento[i] = "vermelho";
+                    }
+                }
                 break;
             case "feijao":
+                for(let i = 0;i < 7; i++){
+                    const windspeedMax = ClimaValidacao.daily?.windspeed_10m_max?.[i] || '0';
+                    if (parseFloat(windspeedMax) < 60.0) {
+                        PerigoVento[i] = "verde";
+                    }
+                    else if (parseFloat(windspeedMax) > 60.0 && parseFloat(windspeedMax) <= 70.0) {
+                        PerigoVento[i] = "amarelo";
+                    }
+                    else {
+                        PerigoVento[i] = "vermelho";
+                    }
+                }
                 break;
         }
 }
@@ -75,12 +122,65 @@ const VerificaTemperatura = async (response:string) => {
             }
             break;
         case "soja":
+            for(let i = 0;i < 7; i++){
+                const tempMax = ClimaValidacao.daily?.temperature_2m_max?.[i] || '0';
+                const tempMin = ClimaValidacao.daily?.temperature_2m_min?.[i] || '0';
+                if (parseFloat(tempMax) > 33.0 || parseFloat(tempMin) < 17.0) {
+                    PerigoTemperatura[i] = "vermelho";
+                }
+                else if (parseFloat(tempMax) > 30.0 || parseFloat(tempMin) < 20.0) {
+                    PerigoTemperatura[i] = "amarelo";
+                }
+                else {
+                    PerigoTemperatura[i] = "verde";
+                }
+            }
             break;
         case "milho":
+            for(let i = 0;i < 7; i++){
+                const tempMax = ClimaValidacao.daily?.temperature_2m_max?.[i] || '0';
+                const tempMin = ClimaValidacao.daily?.temperature_2m_min?.[i] || '0';
+                if (parseFloat(tempMax) > 33.0 || parseFloat(tempMin) < 20.0) {
+                    PerigoTemperatura[i] = "vermelho";
+                }
+                else if (parseFloat(tempMax) > 30.0 || parseFloat(tempMin) < 24.0) {
+                    PerigoTemperatura[i] = "amarelo";
+                }
+                else {
+                    PerigoTemperatura[i] = "verde";
+                }
+            }
+            
             break;
         case "trigo":
+            for(let i = 0;i < 7; i++){
+                const tempMax = ClimaValidacao.daily?.temperature_2m_max?.[i] || '0';
+                const tempMin = ClimaValidacao.daily?.temperature_2m_min?.[i] || '0';
+                if (parseFloat(tempMax) > 26.0 || parseFloat(tempMin) < 12.0) {
+                    PerigoTemperatura[i] = "vermelho";
+                }
+                else if (parseFloat(tempMax) > 20.0 || parseFloat(tempMin) < 15.0) {
+                    PerigoTemperatura[i] = "amarelo";
+                }
+                else {
+                    PerigoTemperatura[i] = "verde";
+                }
+            }
             break;
         case "feijao":
+            for(let i = 0;i < 7; i++){
+                const tempMax = ClimaValidacao.daily?.temperature_2m_max?.[i] || '0';
+                const tempMin = ClimaValidacao.daily?.temperature_2m_min?.[i] || '0';
+                if (parseFloat(tempMax) > 28.0 || parseFloat(tempMin) < 14.0) {
+                    PerigoTemperatura[i] = "vermelho";
+                }
+                else if (parseFloat(tempMax) > 24.0 || parseFloat(tempMin) < 18.0) {
+                    PerigoTemperatura[i] = "amarelo";
+                }
+                else {
+                    PerigoTemperatura[i] = "verde";
+                }
+            }
             break;
     }
 }
@@ -102,12 +202,60 @@ const VerificaChuva = async (response:string) => {
             }
             break;
         case "soja":
+            for(let i = 0;i < 7; i++){
+                const precipitacaoDia = ClimaValidacao.daily?.precipitation_sum?.[i] || '0';
+                if (parseFloat(precipitacaoDia) < 3  || parseFloat(precipitacaoDia) > 7) {
+                    PerigoChuva[i] = "vermelho";
+                }
+                else  if (parseFloat(precipitacaoDia) < 4  || parseFloat(precipitacaoDia) > 6) {
+                    PerigoChuva[i] = "amarelo";
+                }
+                else {
+                    PerigoChuva[i] = "verde";
+                }
+            }
             break;
         case "milho":
+            for(let i = 0;i < 7; i++){
+                const precipitacaoDia = ClimaValidacao.daily?.precipitation_sum?.[i] || '0';
+                if (parseFloat(precipitacaoDia) < 3  || parseFloat(precipitacaoDia) > 7) {
+                    PerigoChuva[i] = "vermelho";
+                }
+                else  if (parseFloat(precipitacaoDia) < 4  || parseFloat(precipitacaoDia) > 6) {
+                    PerigoChuva[i] = "amarelo";
+                }
+                else {
+                    PerigoChuva[i] = "verde";
+                }
+            }
             break;
         case "trigo":
+            for(let i = 0;i < 7; i++){
+                const precipitacaoDia = ClimaValidacao.daily?.precipitation_sum?.[i] || '0';
+                if (parseFloat(precipitacaoDia) < 3  || parseFloat(precipitacaoDia) > 7) {
+                    PerigoChuva[i] = "vermelho";
+                }
+                else  if (parseFloat(precipitacaoDia) < 4  || parseFloat(precipitacaoDia) > 6) {
+                    PerigoChuva[i] = "amarelo";
+                }
+                else {
+                    PerigoChuva[i] = "verde";
+                }
+            }
             break;
         case "feijao":
-            break;
+            for(let i = 0;i < 7; i++){
+            const precipitacaoDia = ClimaValidacao.daily?.precipitation_sum?.[i] || '0';
+            if (parseFloat(precipitacaoDia) < 3  || parseFloat(precipitacaoDia) > 7) {
+                PerigoChuva[i] = "vermelho";
+            }
+            else  if (parseFloat(precipitacaoDia) < 4  || parseFloat(precipitacaoDia) > 6) {
+                PerigoChuva[i] = "amarelo";
+            }
+            else {
+                PerigoChuva[i] = "verde";
+            }
+        }
+        break;
     }
 }
